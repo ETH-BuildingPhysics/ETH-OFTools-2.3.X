590cbd44d6e21bbcf2fd30058bf561a28156e921
mvonlanthen <vonlanthen@arch.ethz.ch>
2015-02-25 17:16:19 +0100

    Initial commit of python tools: generateXDMF.py, saveOFprobesToH5.py and saveOFsurfacesToH5.py

---
+++ python tools/generateXDMF.py
@@ -0,0 +1,208 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# * * * * * * * * * * * * * * * Import modules  * * * * * * * * * * * * * * * #
+
+import re
+import os
+import sys
+import h5py
+import argparse
+import ntpath
+
+# math modules
+import numpy as np
+import scipy as sp
+
+# * * * * * * * * * * * * * * * Functions * * * * * * * * * * * * * * * * * * #
+def pathLeaf(path):
+    '''
+    get trailing file from a path (works on windows and linux)
+    '''
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+
+def getH5metaData(hdf5Parser,varList):
+    '''
+    '''
+    # create the returned variable
+    varsPerTs = dict()
+    varDim = dict()
+    nFaces = int()
+    nPoints = int()
+    
+    
+    # get all the base keys and timestep keys
+    allkeys = hdf5Parser.keys()
+    allkeys.sort()
+    tskeys = allkeys
+    try:
+        tskeys.pop(tskeys.index('mesh'))
+    except:
+        raise ValueError('no "mesh" entry in the HDF5 file. Exit.')
+    
+    # fill nFaces and nPoints
+    nFaces = hdf5Parser['mesh']['faces'].shape[0]
+    nPoints = hdf5Parser['mesh']['points'].shape[0]
+    
+    # fill varPerTs and varDim
+    for ts in tskeys:
+        varlist = hdf5Parser[ts].keys()
+        varlist.pop(varlist.index('time'))
+        if len(args.varList)>0:
+            varsPerTs[ts] = args.varList
+        else:
+            varsPerTs[ts] = varlist
+        for var in varlist:
+            if varDim.has_key(var)==True:
+                pass
+            else:
+                varShape = hdf5Parser[ts][var].shape
+                if len(varShape)>1:
+                    varDim[var] = hdf5Parser[ts][var].shape[1]
+                else:
+                    varDim[var] = 1
+    
+    return varsPerTs, varDim, nFaces, nPoints
+ 
+
+def writeTopology(fileName,nFaces,nTab=0,baseTab=2):
+    '''
+    '''
+    btab = baseTab*' '
+    tab = nTab*btab   
+    lines = (''+tab+'<Topology Type="Triangle" NumberOfElements="'+str(nFaces)+'">\n'
+             ''+tab+btab+'<DataStructure Dimensions="'+str(nFaces)+' 3" NumberType="Int" Format="HDF">\n'
+             ''+tab+2*btab+''+fileName+':/mesh/faces\n'
+             ''+tab+btab+'</DataStructure>\n'
+             ''+tab+'</Topology>\n')       
+    return lines
+
+
+def writeGeometry(fileName,nPoints,nTab=0,baseTab=2):
+    '''
+    '''
+    btab = baseTab*' '
+    tab = nTab*btab 
+    lines = (''+tab+'<Geometry GeometryType="XYZ">\n'
+             ''+tab+btab+'<DataStructure Dimensions="'+str(nPoints)+' 3" NumberType="Float" Presicion="4" Format="HDF">\n'
+             ''+tab+2*btab+''+fileName+':/mesh/points\n'
+             ''+tab+btab+'</DataStructure>\n'
+             ''+tab+'</Geometry>\n')
+    return lines
+
+
+def writeAttribute(fileName,nPoints,time,varName,varDim,nTab=0,baseTab=2):
+    '''
+    '''
+    varType = str()
+    if varDim==1:
+        varType = 'Scalar'
+    elif varDim==3:
+        varType = 'Vector'
+    elif varDim==6:
+        varType = 'Tensor6'
+    elif varDim==9:
+        varType = 'Tensor'
+    btab = baseTab*' '
+    tab = nTab*btab
+    lines = (''+tab+'<Attribute Name="'+varName+'" Center="Node" AttributeType="'+varType+'">\n'
+             ''+tab+btab+'<DataStructure Format="HDF" DataType="Float" Precision="4" Dimensions="'+str(nPoints)+' '+str(varDim)+'">\n'
+             ''+tab+2*btab+''+fileName+':/'+time+'/'+varName+'\n'
+             ''+tab+btab+'</DataStructure>\n'
+             ''+tab+'</Attribute>\n')
+    return lines
+   
+   
+# * * * * * * * * * * * * * Parse arguments * * * * * * * * * * * * * * * * * #
+toolDescription = ('Create the xdmf file from a HDF5 file of surfaces. '
+                   'Works only with TruSurface HDF5. the HDF5 can be created '
+                   'with the tool "saveOFsurfacesToH5.py".')
+                   
+parser = argparse.ArgumentParser(description=toolDescription)
+
+parser.add_argument('-h5',
+                    '--h5file',
+                    dest='h5File',
+                    type=str,
+                    required=True,
+                    help='Path to the HDF5 file.'
+                    )
+                    
+parser.add_argument('-vl',
+                    '--varlist',
+                    dest='varList',
+                    type=str,
+                    required=False,
+                    nargs='+',
+                    default=list(),
+                    help='Specify variables to include in the xdmf file.'
+                    )
+
+args = parser.parse_args()
+
+# * * * * * * * * * * * * * * * * Main * * * * * * * * * * * * * * * * * * * #
+workingDir = os.getcwd()
+
+absH5File = os.path.abspath(args.h5File)
+h5FilePath = os.path.dirname(absH5File)
+h5FileName = pathLeaf(absH5File)
+
+xdmfFileName = h5FileName[:-3]+'.xdmf'
+xdmfFileName = os.path.splitext(h5FileName)[0]+'.xdmf'
+absXdmfFile = os.path.join(h5FilePath,xdmfFileName)
+
+
+# create HDF5 parser
+fr = h5py.File(absH5File, 'r')
+varsPerTs, varDim, nFaces, nPoints = getH5metaData(hdf5Parser=fr,varList=args.varList)
+fr.close()
+
+# tabulation for the entire file
+baseTab = 2
+
+# tab size for the header and the bottom lines
+btab = baseTab*' '
+
+# start writing the file 
+fo = open(absXdmfFile, 'w')
+
+# write the header lines
+fo.write('<Xdmf>\n')
+fo.write(''+btab+'<Domain>\n')
+fo.write(''+2*btab+'<Grid Name="FieldData" GridType="Collection" CollectionType="Temporal">\n')
+fo.write('\n')
+
+# write rhe body
+allTs = varsPerTs.keys()
+allTs.sort()
+for tsStr in allTs:
+    tab = 2*btab
+    fo.write(''+tab+'<Grid GridType="Collection" CollectionType="Spatial">\n')
+    fo.write(''+tab+'<Time Type="Single" Value="'+tsStr+'" />\n')
+    fo.write(''+btab+tab+'<Grid Name="'+tsStr+'" Type="Uniform">\n')    
+    fo.writelines(writeTopology(fileName=h5FileName,nFaces=nFaces,nTab=4,baseTab=baseTab)) 
+    fo.writelines(writeGeometry(fileName=h5FileName,nPoints=nPoints,nTab=4,baseTab=baseTab))
+    for var in varsPerTs[tsStr]:
+        fo.writelines(writeAttribute(fileName=h5FileName,
+                                     nPoints=nPoints,
+                                     time=tsStr,
+                                     varName=var,
+                                     varDim=varDim[var],
+                                     nTab=4,
+                                     baseTab=baseTab))
+    fo.write(''+btab+tab+'</Grid>\n')
+    fo.write(''+tab+'</Grid>\n') 
+    fo.write('\n')
+
+# write the bottom lines
+fo.write('\n')
+fo.write(''+2*btab+'</Grid>\n')
+fo.write(''+btab+'</Domain>\n')
+fo.write('</Xdmf>\n')
+
+fo.close()
+
+# * * * * * * * * * * * * * * * * End * * * * * * * * * * * * * * * * * * * * #
---
+++ python tools/saveOFprobesToH5.py
@@ -0,0 +1,258 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+#=============================================================================#
+# Import modules
+#=============================================================================#
+
+import re
+import os
+import sys
+import time
+import h5py
+import argparse
+
+# math modules
+import numpy as np
+import scipy as sp
+import scipy.linalg as spla
+import scipy.signal as spsi
+import math as math
+
+# mpl.use('QT4Agg')         #load qt4 backend (optional)
+
+import pyFlowStat.PointProbe as pp
+import pyFlowStat.PointProbeFunctions as ppf
+import pyFlowStat.TurbulenceTools as tt
+import pyFlowStat.TriSurface as ts
+
+#=============================================================================#
+# Functions
+#=============================================================================#
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+def loadOFProbeList(relpath):
+    absPath = os.path.abspath(relpath)
+    if os.path.exists(absPath):
+        print('    loading file "'+str(relpath)+'"')
+        return pp.getOFPointProbeList(absPath,reshape=True,createDict=False)
+    else:
+        sys.exit('Path exist: False. Script abort.')
+
+
+def appendPPList(runList,createDict):
+    runs = []
+    
+    # load all runs
+    for run in runList:
+        runs.append(loadOFProbeList(run))
+           
+    # append runs to the first one of the list
+    for runi in range(1,len(runs)):
+        print('    appending data from file '+str(runi)+' to data from file 0')
+        for pti in range(len(runs[runi])):
+            runs[0][pti].appendProbe(runs[runi][pti],rmOverlap='self',createDict=False)    
+    
+    if createDict==True:
+        print('    create data dict')
+        for pt in runs[0]:        
+            pt.createDataDict(action=createDict)
+            
+    return runs[0]
+
+def getSubDirList(dir):
+    return [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
+
+
+#=============================================================================#
+# parse arguments
+#=============================================================================#
+parser = argparse.ArgumentParser(description=('Convert an OpenFOAM probe file in a hdf5 probe file. ',
+                                              'The OpenFOAM probes must be located in the OF default ',
+                                              'path (ex: OFcase/postProcessing/probeName/timestep/U)'))
+parser.add_argument('-case',
+                    dest='case',
+                    type=str,
+                    required=False,
+                    default=os.getcwd(),
+                    help='Path of an OpenFOAM case. Default: execution path'
+                    )
+parser.add_argument('-probename',
+                    dest='probename',
+                    type=str,
+                    required=True,
+                    help='name of the probe to convert. Must be located in postProcessing'
+                    )
+parser.add_argument('-region',
+                    dest='region',
+                    type=str,
+                    required=False,
+                    default=str(),
+                    help='specifiy a different region name. Default: region0 (the default OpenFOAM region)'
+                    )
+parser.add_argument('-keyrange',
+                    dest='keyrange',
+                    type=str,
+                    required=False,
+                    choices=['raw','full'],
+                    default='raw',
+                    help=('Specify which datas must be saved in the hdf5. ',
+                          'Choices: raw or full.  Default: raw. See ',
+                          'pyFlowStat.PointProbeFunctions.savePPlist_hdf5 for more details.')
+                    )
+parser.add_argument('-var',
+                    dest='var',
+                    type=str,
+                    required=True,
+                    nargs='+',
+                    help='Specify variables to save in hdf5.'
+                    )
+parser.add_argument('-rename',
+                    dest='rename',
+                    type=str,
+                    required=False,
+                    default='',
+                    help=('Specify an extended name for all the h5 files generated. ',
+                          'Example -rename foobar --> U_foobar.h5')
+                    )
+parser.add_argument('-overwrite',
+                    dest="overwrite",
+                    action="store_true",
+                    default=False,
+                    help=('if the hdf5 file already exist, delete it and recreate it. ',
+                          'Default behavior: the file already exists, nothing is ',
+                          'saved and the execution continues with the other variable')
+                    )
+
+args = parser.parse_args()
+
+
+#=============================================================================#
+# Main
+#=============================================================================#
+#check if case exist and get absolute path
+case = args.case
+caseAbsPath = str()
+if os.path.exists(case):
+    caseAbsPath = os.path.abspath(case)
+else:
+    sys.exit('Case "'+case+'" does not exist. Check your input. Exit')
+
+
+# check if probename exist and get absolute path
+probename = args.probename
+probenameAbsPath = os.path.join(caseAbsPath,'postProcessing',probename,args.region)
+if os.path.exists(probenameAbsPath):
+    pass
+else:
+    sys.exit('Probename "'+probename+'" does not exist. Check your input. Exit')
+
+
+# get list of time step in probename and list of time step absolute path
+dirList = getSubDirList(probenameAbsPath)
+tsList = []
+for d in dirList:
+    if is_number(d)==True:
+        tsList.append(d)
+tsList.sort()
+tsList_absPath = [os.path.join(probenameAbsPath,tstep) for tstep in tsList]
+
+
+# get list of all variable in all time steps
+varList_raw = []
+for i in range(len(tsList)):
+    names = [name for name in os.listdir(tsList_absPath[i]) if os.path.isfile(os.path.join(tsList_absPath[i], name))]
+    for name in names:
+        varList_raw.append(name)
+varList = list(set(varList_raw))
+
+
+# create a dict with the vars of varList has key, and the list of location containing the vars.
+varLocDict = dict()
+for var in varList:
+    varLocDict[var] = []
+
+for i in range(len(tsList)):
+    names = [name for name in os.listdir(tsList_absPath[i]) if os.path.isfile(os.path.join(tsList_absPath[i], name))]
+    for name in names:
+        varLocDict[name].append(os.path.join(tsList_absPath[i],name))
+
+#-----------------------------------------------------------------------------#
+# save all probed varaible in a hdf5 file
+#-----------------------------------------------------------------------------#
+varToSave = []
+#if args.var==None:
+#    for key in varLocDict.keys():
+#        varToSave.append(key)
+#else:
+#    for key in args.var:
+#        if varLocDict.has_key(key)==True:
+#            varToSave.append(key)
+#        else:
+#            sys.exit('variable "'+key+'" does not exist. Check your input. Exit')
+
+for key in args.var:
+    if varLocDict.has_key(key)==True:
+        varToSave.append(key)
+    else:
+        sys.exit('variable "'+key+'" does not exist. Check your input. Exit')
+
+print('case:')
+print('    '+caseAbsPath)
+print('')
+
+print('probe name:')
+print('    '+probenameAbsPath)
+print('')
+
+print('keyrange:')
+print('    '+args.keyrange)
+print('')
+
+print('list of variables converted from OpenFOAM to hdf5:')
+for var in varToSave:
+    print('    '+var)
+print('')
+
+for key in varToSave:
+    
+    # load data in memory
+    print('start converting and saving variable '+key+':')
+    createDict = bool()
+    if args.keyrange=='full':
+        createDict = True
+    else:
+        createDict = False
+    keyData = appendPPList(varLocDict[key],createDict=createDict)
+    
+    # save data in HDF5
+    h5TgtFile = str()
+    if args.rename=='':
+        h5TgtFile = os.path.join(probenameAbsPath, key+'.h5')
+    else:
+        h5TgtFile = os.path.join(probenameAbsPath, key+'_'+args.rename+'.h5')
+            
+    
+    if os.path.exists(h5TgtFile)==True and args.overwrite==True:
+        os.remove(h5TgtFile)
+        print('    Saving variable "'+key+'" in "'+h5TgtFile+'"')
+        ppf.savePPlist_hdf5(ppList=keyData, hdf5file=h5TgtFile, keyrange=args.keyrange)
+
+    elif os.path.exists(h5TgtFile)==False:
+        print('    Saving variable "'+key+'" in "'+h5TgtFile+'"')
+        ppf.savePPlist_hdf5(ppList=keyData, hdf5file=h5TgtFile, keyrange=args.keyrange)
+
+    elif  os.path.exists(h5TgtFile)==True and args.overwrite==False:
+        print('file"'+h5TgtFile+'" already exists. Execution continues with the next variable.')
+        
+    print('')
+
+print('')
+
